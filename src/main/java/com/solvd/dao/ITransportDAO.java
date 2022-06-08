64fae046bc6c86877850985f30f6d381db3e04c6
laurot <laurot275@gmail.com>
2022-06-08 17:44:24 -0300

    minor change to interface

--- src/main/java/com/solvd/dao/ITransportDAO.java
+++ src/main/java/com/solvd/dao/ITransportDAO.java
@@ -1,7 +1,9 @@
 package com.solvd.dao;
 
+import java.util.List;
+
 import com.solvd.bin.Transport;
 
 public interface ITransportDAO extends IBaseDAO<Transport>{
-    
+    List<Transport> getAllTransports();
 }
