54587a6fcd4b499adf50e07454a316a69a84a86a
Alexisvi14 <alexisvillamayor96>
2022-06-08 22:27:11 -0300

    some utils were added

---
+++ src/main/java/com/solvd/util/Constants.java
@@ -0,0 +1,14 @@
+package com.solvd.util;
+
+public class Constants {
+
+    public static String MAX_CONNECTIONS = "db.max_connections";
+
+    public static String URL = "db.url";
+
+    public static String USERNAME = "db.username";
+
+    public static String PASSWORD = "db.pass";
+
+    public static String DRIVER = "db.driver";
+}
